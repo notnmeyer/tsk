353e9a7f6c62b07a1bfe8e2073ba1327ccbdbdf3
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2024-12-31 13:25:24 -0800

    undo my hardcoded test binary

--- completions/tsk.fish
+++ completions/tsk.fish
@@ -1,8 +1,6 @@
 function __fish_tsk_list_tasks
-    # take the task name and the first line in the description and format them:
-    # <name>TAB<desc>
-    command /Users/nate/code/tsk/bin/tsk -l --output json 2>/dev/null \
-        | jq -r 'to_entries[] | [.key, .value.Desc] | @tsv'
+    # take the task name and the first line in the description and format them: <name>TAB<desc>
+    command tsk -l --output json 2>/dev/null | jq -r 'to_entries[] | [.key, .value.Desc] | @tsv'
 end
 
 complete \
