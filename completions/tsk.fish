42d53d8e922c6297f864e47ced118e501a1cf739
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2024-12-31 13:20:32 -0800

    update completions script to use jq and simplify the command

--- completions/tsk.fish
+++ completions/tsk.fish
@@ -1,15 +1,8 @@
 function __fish_tsk_list_tasks
     # take the task name and the first line in the description and format them:
     # <name>TAB<desc>
-    command tsk -l --output toml 2>/dev/null \
-        | tomlq -r '
-            to_entries[]
-            | [ .key, (
-                .value.description // ""
-                | split("\n")[0]
-              ) ]
-            | @tsv
-          '
+    command /Users/nate/code/tsk/bin/tsk -l --output json 2>/dev/null \
+        | jq -r 'to_entries[] | [.key, .value.Desc] | @tsv'
 end
 
 complete \
