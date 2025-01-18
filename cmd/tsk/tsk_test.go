63dd45dece3a633ba2643434880fa813e97d62fd
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2025-01-17 22:04:29 -0800

    add tests for parseArgs()

---
+++ cmd/tsk/tsk_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseArgs(t *testing.T) {
+	tests := []struct {
+		name            string
+		input           []string
+		expectedTasks   []string
+		expectedCliArgs string
+	}{
+		{
+			name:            "tasks and args",
+			input:           []string{"task1", "task2", "--", "arg1", "arg2"},
+			expectedTasks:   []string{"task1", "task2"},
+			expectedCliArgs: "arg1 arg2",
+		},
+		{
+			name:            "just tasks",
+			input:           []string{"task1", "task2"},
+			expectedTasks:   []string{"task1", "task2"},
+			expectedCliArgs: "",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tasks, cliArgs := parseArgs(test.input)
+			if !equalSlices(tasks, test.expectedTasks) {
+				t.Errorf("Expected tasks: %v, got: %v", test.expectedTasks, tasks)
+			}
+			if cliArgs != test.expectedCliArgs {
+				t.Errorf("Expected cliArgs: %q, got: %q", test.expectedCliArgs, cliArgs)
+			}
+		})
+	}
+}
+
+func equalSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
